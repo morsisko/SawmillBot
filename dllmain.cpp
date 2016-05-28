e8abbddaadf04b82c3ac2b20053fd9df8f817d2a
morsisko <morsisko@gmail.com>
2016-05-28 22:14:29 +0200

    comments in code

--- dllmain.cpp
+++ dllmain.cpp
@@ -2,7 +2,6 @@
 #include <Windows.h>
 #include "Structures.h"
 #include "PatternScanner.h"
-#include <random>
 
 #define KEY_DOWN 0x100
 #define KEY_UP 0x101
@@ -14,26 +13,35 @@
 
 DWORD WINAPI DLLStart(LPVOID param)
 {
+	//Get pointer that points to TMiniGameManager pointer
 	TMiniGameManager* manager = **(TMiniGameManager***)(FindPattern((BYTE*)0x500000, 
 														0x200000, 
 														(BYTE*)("\xA1\x00\x00\x00\x00\x8B\x08\xFF\x51\x44\xA1\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x33\xD2"), "x????xxxxxx????x????xx") + 1);
 
 	MiniGame_1* game = manager->miniGameManagerList->getMiniGame();
 
-	if (IsBadReadPtr(game, sizeof(game)))
+	//Check if the pointer to MiniGame_1 is correct
+	if (IsBadReadPtr(game, sizeof(MiniGame_1)))
 		return -1;
 
+	//Get the paddles data
 	void* leftPaddleData = game->leftPaddle->data;
 	void* rightPaddleData = game->rightPaddle->data;
 
+	//Find hwnd of NosTale window
 	HWND hwnd = FindWindowA("TNosTaleMainF", "NosTale");
 
 	while (true)
 	{
+		//Chceck if minigame is on
 		if (game->m_bIsOn)
 		{
 
-			for (int i = 397; i != 327; --i)
+			//Paddle hitbox area(the place where we can hit) has 110 bytes (397-287).
+			//If the wood is between byte 40 and byte 70 we get the "Nice" result, and as a result, the "combo x"
+			//Really the front of the paddle is at the back of memory, so at offset 397 is the start of the paddle, and at 287 is the end of paddle
+			//We iterate the last(really first) 70 bytes, because we don't want to lose wood, when it will be at the byte lower than 40, but we still prefer to get combo
+			for (int i = 397; i != 327; --i) // 287 max
 			{
 				if (*(BYTE*)((DWORD)leftPaddleData + (DWORD)i) == WOOD)
 				{
